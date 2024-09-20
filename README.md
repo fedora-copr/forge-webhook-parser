cc8b47bd95fb0d81697021cea1f84a279274150f
Don Naro <dnaro@redhat.com>
2024-09-20 09:49:20 +0100

    Add checks (#4)
    * git ignore file
    
    * pip tools config for generating lock files
    
    * requirements for checkers and formatters
    
    * nox sessions to automate checks
    
    * add ci workflows
    
    * add docs for nox sessions and dependencies

--- README.md
+++ README.md
@@ -4,4 +4,70 @@
 
 Library for parsing webhook payloads from popular git forges
 
-Under construction
+## Verifying your pull request
+
+We welcome all contributions.
+If you plan to submit a pull request with changes, you should run the automated tests to check for issues.
+
+### Setting up nox
+
+This project includes a `nox` configuration to automate tests, checks, and other functions.
+You can use these automated tests to help you verify changes before you submit a PR.
+
+* Install `nox` using `python3 -m pip install nox` or your distribution's package manager.
+
+* Execute `nox` from the repository root with no arguments to run all Python code checkers.
+
+* Alternatively, you can run only certain tasks as outlined in the following sections.
+  Run `nox --list` to view available sessions.
+
+### Running automated tests
+
+* Perform static analysis with [ruff](https://docs.astral.sh/ruff/).
+
+  ``` bash
+  nox -s static
+  ```
+
+* Reformat code with [black](https://black.readthedocs.io/en/stable/) and [isort](https://pycqa.github.io/isort/).
+
+  ``` bash
+  nox -s formatters
+  ```
+
+* Check code for formatting issues without applying changes.
+
+  ``` bash
+  nox -s formatters_check
+  ```
+
+* Perform static type checking with [mypy](https://mypy.readthedocs.io/en/stable/).
+
+  ``` bash
+  nox -s typing
+  ```
+
+## Dependency files
+
+`nox` sessions use dependencies from requirements files in the `requirements/` directory.
+Each session has a `requirements/*.in` file with direct dependencies and a lock file in `requirements/*.txt` that pins exact versions for both direct and transitive dependencies.
+
+* Use the following `nox` session to update all dependency lock files:
+
+  ``` bash
+  nox -s pip-compile
+  ```
+
+* Update specific dependency lock files with individual `nox` sessions, for example:
+
+  ``` bash
+  nox -s "pip-compile-3.12(formatters)"
+  ```
+
+By default, the `nox` sessions upgrade dependencies.
+You can pass `pip-compile` options `--no-upgrade` and `--upgrade-package` to update only specific packages and keep other dependencies at their current version.
+For example, to upgrade the `isort` package only:
+
+``` bash
+nox -s "pip-compile-3.12(formatters)" -- --no-upgrade --upgrade-package=isort
+```
---
+++ noxfile.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+
+import nox
+
+LINT_FILES = ["forge-webhook-parser.py"]
+
+requirements_directory = Path("requirements")
+
+requirements_files = [
+    requirements_input_file_path.stem
+    for requirements_input_file_path in requirements_directory.glob("*.in")
+]
+
+
+@nox.session(name="pip-compile", python=["3.12"])
+@nox.parametrize(["req"], arg_values_list=requirements_files, ids=requirements_files)
+def pip_compile(session: nox.Session, req: str):
+    """Generate lock files from input files or upgrade packages in lock files."""
+    # fmt: off
+    session.install(
+      "-r", str(requirements_directory / "pip-tools.in"),
+      "-c", str(requirements_directory / "pip-tools.txt"),
+    )
+    # fmt: on
+
+    # Use --upgrade by default unless a user passes -P.
+    upgrade_related_cli_flags = ("-P", "--upgrade-package", "--no-upgrade")
+    has_upgrade_related_cli_flags = any(
+        arg.startswith(upgrade_related_cli_flags) for arg in session.posargs
+    )
+    injected_extra_cli_args = () if has_upgrade_related_cli_flags else ("--upgrade",)
+
+    session.run(
+        "pip-compile",
+        "--output-file",
+        str(requirements_directory / f"{req}.txt"),
+        *session.posargs,
+        *injected_extra_cli_args,
+        str(requirements_directory / f"{req}.in"),
+    )
+
+
+def install(session: nox.Session, req: str):
+    session.install(
+        "-r",
+        str(requirements_directory / f"{req}.in"),
+        "-c",
+        str(requirements_directory / f"{req}.txt"),
+    )
+
+
+@nox.session
+def static(session: nox.Session):
+    """
+    Run static checkers
+    """
+    install(session, req="static")
+    session.run("ruff", "check", *session.posargs, *LINT_FILES)
+
+
+@nox.session
+def formatters(session: nox.Session):
+    """
+    Reformat code
+    """
+    install(session, req="formatters")
+    session.run("isort", *session.posargs, *LINT_FILES)
+    session.run("black", *session.posargs, *LINT_FILES)
+
+
+@nox.session
+def formatters_check(session: nox.Session):
+    """
+    Check code formatting without making changes
+    """
+    install(session, req="formatters")
+    session.run("isort", "--check", *session.posargs, *LINT_FILES)
+    session.run("black", "--check", *session.posargs, *LINT_FILES)
+
+
+@nox.session
+def typing(session: nox.Session):
+    """
+    Run static type checker
+    """
+    install(session, req="typing")
+    session.run("mypy", *session.posargs, *LINT_FILES)
