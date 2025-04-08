1a2bb198611db1eb76bd3278a16e1015354b934e
Don Naro <dnaro@redhat.com>
2025-04-08 14:36:37 +0100

    attempt to simplify things + url tests

--- tests/test_parser_functions.py
+++ tests/test_parser_functions.py
@@ -1,5 +1,5 @@
 import json
-from unittest.mock import MagicMock, mock_open, patch
+from unittest.mock import patch
 
 from forge_webhook_parser import load_json_data, parse_arguments
 
@@ -20,35 +20,48 @@ class TestParseArguments:
             assert args.file is None
 
 
-class TestLoadJsonData:
-    @patch("builtins.open", new_callable=mock_open)
-    def test_load_from_file_github(self, mock_file, github_payload):
+class TestLoadJsonDataFile:
+    def test_load_from_file_github(self, mock_args_file, github_payload):
+        with patch("builtins.open") as mock_open:
+            mock_file = mock_open.return_value.__enter__.return_value
+            mock_file.read.return_value = json.dumps(github_payload)
 
-        mock_file.return_value.__enter__.return_value.read.return_value = json.dumps(
-            github_payload
-        )
+            data = load_json_data(mock_args_file)
 
-        args = MagicMock()
-        args.file = "payload.json"
-        args.url = None
+            assert data == github_payload
+            mock_open.assert_called_once_with("payload.json", "r", encoding="utf-8")
 
-        data = load_json_data(args)
+    def test_load_from_file_gitlab(self, mock_args_file, gitlab_payload):
+        with patch("builtins.open") as mock_open:
+
+            mock_file = mock_open.return_value.__enter__.return_value
+            mock_file.read.return_value = json.dumps(gitlab_payload)
+
+            data = load_json_data(mock_args_file)
+
+            assert data == gitlab_payload
+            mock_open.assert_called_once_with("payload.json", "r", encoding="utf-8")
+
+
+class TestLoadJsonDataUrl:
+    @patch("requests.get")
+    def test_load_from_url_github(self, mock_get, mock_args_url, github_payload):
+
+        mock_response = mock_get.return_value
+        mock_response.json.return_value = github_payload
+
+        data = load_json_data(mock_args_url)
 
         assert data == github_payload
-        mock_file.assert_called_once_with("payload.json", "r", encoding="utf-8")
+        mock_get.assert_called_once_with("http://example.com/payload.json")
 
-    @patch("builtins.open", new_callable=mock_open)
-    def test_load_from_file_gitlab(self, mock_file, gitlab_payload):
+    @patch("requests.get")
+    def test_load_from_url_gitlab(self, mock_get, mock_args_url, gitlab_payload):
 
-        mock_file.return_value.__enter__.return_value.read.return_value = json.dumps(
-            gitlab_payload
-        )
+        mock_response = mock_get.return_value
+        mock_response.json.return_value = gitlab_payload
 
-        args = MagicMock()
-        args.file = "payload.json"
-        args.url = None
-
-        data = load_json_data(args)
+        data = load_json_data(mock_args_url)
 
         assert data == gitlab_payload
-        mock_file.assert_called_once_with("payload.json", "r", encoding="utf-8")
+        mock_get.assert_called_once_with("http://example.com/payload.json")
